2e3dc47bda8d981c0162c69048926888b27e8e9b
Artem Lipatkin <lipatkin96@gmail.com>
2018-10-29 22:15:17 +0700

    Add controlling to game (tennis).

--- Game/tennis.pas
+++ Game/tennis.pas
@@ -11,8 +11,28 @@ var ball : SphereT;
 
 begin
 
-  roketka1 := Box(0,-5,0.5, DLINA_ROKETKA,1,1,Colors.Orange);
-  roketka2 := Box(0, 5,0.5, DLINA_ROKETKA,1,1,Colors.Green);
-  ball := Sphere(0, 0, 0.5, BALL_RADIUS, Materials.Rainbow);
+  // Создаём объекты сцены
+  // Первая рокетка:
+  //              координаты-длина рокетки--ширина-высота-цвет
+  roketka1 := Box(0,-5, 0.5, DLINA_ROKETKA, 1,     1,     Colors.Orange);
+  roketka2 := Box(0, 5, 0.5, DLINA_ROKETKA, 1,     1,     Colors.Green);
+  ball :=  Sphere(0, 0, 0.5, BALL_RADIUS,                 Materials.Rainbow);
+  
+  // Присваиваем событию "Нажатие клавиши" определённое действие
+  OnKeyDown := k -> 
+    case k of
+      Key.Left:
+        if (roketka1.X < 5) then
+          roketka1.MoveOn(1,0,0);
+      Key.Right: 
+        if (roketka1.X > -5) then
+          roketka1.MoveOn(-1,0,0);
+      Key.A: 
+        if (roketka2.X < 5) then
+          roketka2.MoveOn(1,0,0);
+      Key.D: 
+        if (roketka2.X > -5) then
+          roketka2.MoveOn(-1,0,0);
+    end;
   
 end.
