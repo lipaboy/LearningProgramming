a42c9dfdb701f2fee8ea9ced63d8b2296407f178
Artem Lipatkin <lipatkin96@gmail.com>
2018-10-29 21:55:42 +0700

    (Tennis) Add two rackets and ball on scene.

--- Game/tennis.pas
+++ Game/tennis.pas
@@ -1,8 +1,18 @@
-﻿uses GraphABC;
- 
-begin
-  for var x:=0 to Window.Width-1 do
-  for var y:=0 to Window.Height-1 do
-    SetPixel(x,y,RGB(2*x-y,x-3*y,x+y));
-end.
+﻿uses Graph3D;
 
+// константы (инварианты или неизменяемые переменные)
+const 
+DLINA_ROKETKA = 3;
+BALL_RADIUS = 0.5;
+
+// переменные
+var roketka1, roketka2 : BoxT;
+var ball : SphereT;
+
+begin
+
+  roketka1 := Box(0,-5,0.5, DLINA_ROKETKA,1,1,Colors.Orange);
+  roketka2 := Box(0, 5,0.5, DLINA_ROKETKA,1,1,Colors.Green);
+  ball := Sphere(0, 0, 0.5, BALL_RADIUS, Materials.Rainbow);
+  
+end.
