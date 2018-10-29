933e70deced09c422196c1cd46a9f04191c9ad49
Artem Lipatkin <lipatkin96@gmail.com>
2018-10-29 23:20:16 +0700

    Добавил условие на проигрыш в игре Теннис.

--- Game/tennis.pas
+++ Game/tennis.pas
@@ -13,62 +13,13 @@ GRANIZA_Y2 = -5;
 var roketka1, roketka2 : BoxT;
 var ball : SphereT;
 var vx, vy, speed, ugol : real;
-  var duration, t : integer;
-
-procedure pusk();
-
-begin
-
-  // Создаём объекты сцены
-  // Первая рокетка:
-  //              координаты---------длина рокетки--ширина-высота-цвет
-  roketka1 := Box(0,GRANIZA_Y2-1, 0.5, DLINA_ROKETKA, 1,     1,     Colors.Orange);
-  roketka2 := Box(0,GRANIZA_Y1+1, 0.5, DLINA_ROKETKA, 1,     1,     Colors.Green);
-  ball :=  Sphere(0, 0, 0.5, BALL_RADIUS,                 Materials.Rainbow);
-  
-  // Присваиваем событию "Нажатие клавиши" определённое действие
-  OnKeyDown := k -> 
-    case k of
-      Key.Left:
-        if (roketka1.X < GRANIZA_X1) then
-          roketka1.MoveOn(1,0,0);
-      Key.Right: 
-        if (roketka1.X > GRANIZA_X2) then
-          roketka1.MoveOn(-1,0,0);
-      Key.A: 
-        if (roketka2.X < GRANIZA_X1) then
-          roketka2.MoveOn(1,0,0);
-      Key.D: 
-        if (roketka2.X > GRANIZA_X2) then
-          roketka2.MoveOn(-1,0,0);
-    end;
-  
-  sleep(1000);
-  
-  duration := 10;
-  vx := 0.02;
-  vy := 0.03;
-  t := 0;
-  
-  // 1 способ
-  
-//  while (true) do
-//  begin
-//    sleep(duration);
-//    pusk();
-//  end;
-  
-  // 2 способ
-  
-  var t1 := new Timer(duration, pusk);
-  t1.Start;
-  
-end.
+var duration : integer;
+var t1 : Timer;
 
 procedure pusk();
 begin
-    ball.X := ball.X + vx * t / 1000.0;
-    ball.Y := ball.Y + vy * t / 1000.0;
+    ball.X := ball.X + vx * duration / 1000.0;
+    ball.Y := ball.Y + vy * duration / 1000.0;
     
     if (ball.X >= GRANIZA_X1) then
     begin
@@ -80,16 +31,73 @@ begin
       vx := -vx;
       ball.X := GRANIZA_X2;
     end;
+    // зона второй ракетки
     if (ball.Y >= GRANIZA_Y1) then
     begin
       vy := -vy;
       ball.Y := GRANIZA_Y1;
+      
+      // условие на проигрыш второго игрока
+      if (ball.X > roketka2.X + DLINA_ROKETKA / 2) 
+        or (ball.X < roketka2.X - DLINA_ROKETKA / 2) then
+          t1.Stop;
     end;
+    // зона первой ракетки
     if (ball.Y <= GRANIZA_Y2) then
     begin
       vy := -vy;
       ball.Y := GRANIZA_Y2;
+      
+      // условие на проигрыш первого игрока
+      if (ball.X > roketka1.X + DLINA_ROKETKA / 2) 
+        or (ball.X < roketka1.X - DLINA_ROKETKA / 2) then
+          t1.Stop;
     end;
-    
-    t := Milliseconds - t;
 end;
+
+begin
+
+  // Создаём объекты сцены
+  // Первая рокетка:
+  //              координаты---------длина рокетки--ширина-высота-цвет
+  roketka1 := Box(0,GRANIZA_Y2-0.8, 0.5, DLINA_ROKETKA, 1,     1,     Colors.Orange);
+  roketka2 := Box(0,GRANIZA_Y1+0.8, 0.5, DLINA_ROKETKA, 1,     1,     Colors.Green);
+  ball :=  Sphere(0, 0, 0.5, BALL_RADIUS,                 Materials.Rainbow);
+  
+  // Присваиваем событию "Нажатие клавиши" определённое действие
+  OnKeyDown := k -> 
+    case k of
+      Key.Up:
+        if (roketka1.X < GRANIZA_X1) then
+          roketka1.MoveOn(1,0,0);
+      Key.Down: 
+        if (roketka1.X > GRANIZA_X2) then
+          roketka1.MoveOn(-1,0,0);
+      Key.W: 
+        if (roketka2.X < GRANIZA_X1) then
+          roketka2.MoveOn(1,0,0);
+      Key.S: 
+        if (roketka2.X > GRANIZA_X2) then
+          roketka2.MoveOn(-1,0,0);
+    end;
+  
+  sleep(1000);
+  
+  duration := 10;
+  vx := 2 * 2;
+  vy := 3 * 2;
+  
+  // 1 способ
+  
+//  while (true) do
+//  begin
+//    sleep(duration);
+//    pusk();
+//  end;
+  
+  // 2 способ
+  
+  t1 := new Timer(duration, pusk);
+  t1.Start;
+  
+end.
